aa191f047d17393cb493583d321a0e1c3ac2af4a
Peter Ingram <peter.ingram0@gmail.com>
2018-06-21 20:37:01 +0100

    testing

--- bitbucket-pipelines-go.sh
+++ bitbucket-pipelines-go.sh
@@ -1,4 +1,4 @@
-BASE_PATH="${GOPATH}/src/bitbucket.org/${BITBUCKET_REPO_OWNER}"
-mkdir -p ${BASE_PATH}
-export IMPORT_PATH="${BASE_PATH}/${BITBUCKET_REPO_SLUG}"
-ln -s ${PWD} ${IMPORT_PATH}
+# BASE_PATH="${GOPATH}/src/bitbucket.org/${BITBUCKET_REPO_OWNER}"
+# mkdir -p ${BASE_PATH}
+# export IMPORT_PATH="${BASE_PATH}/${BITBUCKET_REPO_SLUG}"
+# ln -s ${PWD} ${IMPORT_PATH}
